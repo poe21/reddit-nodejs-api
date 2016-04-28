d714d99e9229cc95a35c7f083fa362c8e0116ea5
Annie <annie-d@hotmail.ca>
2016-04-28 21:41:31 +0000

    added create table subreddits mysql queries

--- reddit.sql
+++ reddit.sql
@@ -17,9 +17,27 @@ CREATE TABLE `posts` (
   `title` varchar(300) DEFAULT NULL,
   `url` varchar(2000) DEFAULT NULL,
   `userId` int(11) DEFAULT NULL,
+  `subredditId` int(11) DEFAULT NULL,
   `createdAt` TIMESTAMP NOT NULL DEFAULT 0,
   `updatedAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `userId` (`userId`),
+  KEY `subredditId` (`subredditId`),
   CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE SET NULL
+  CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`subredditId`) REFERENCES `subreddits` (`id`) ON DELETE SET NULL  -- subreddits foreign keys
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Each subreddit should have a unique, auto incrementing id,
+-- a name anywhere from 1 to 30 characters, and an optional description of up to 200 
+-- characters. Each sub should also have createdAt and updatedAt timestamps that you can copy from an existing 
+-- table. To guarantee the integrity of our data, we should make sure that the name column is unique.
+
+CREATE TABLE `subreddits` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(30) DEFAULT NULL,
+  `description` VARCHAR(200) DEFAULT NULL,
+  `createdAt` TIMESTAMP NOT NULL DEFAULT 0,
+  `updatedAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
